4fc166d1c56b832d1cca6260054cf32c61501803
Long29082003 <longd12321@gmail.com>
2026-03-14 22:41:14 -0500

    1. Finish the UI for register page

--- client/src/routes/register/Register.jsx
+++ client/src/routes/register/Register.jsx
@@ -1,58 +1,127 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
+import { IoIosWarning } from "react-icons/io";
 import { FaCheckCircle } from "react-icons/fa";
-import { BiErrorAlt } from "react-icons/bi";
+import { FaCircleXmark } from "react-icons/fa6";
 import { LuInfo } from "react-icons/lu";
 
 import { Button } from "../home/utilcomponents/button.jsx";
 
 import "./Register.css";
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 export function Register () {
+    //? States
     const [ error, setError ] = useState("");
     const [ registerStatus, setRegisterStatus ] = useState(null);
 
+    const [ username, setUsername ] = useState("");
+    const isUsernameValid = USER_REGEX.test(username);
+
+    const [ password, setPassword ] = useState("");
+    const isPasswordValid = PWD_REGEX.test(password);
+
+    const [ isMatchingPwdOnFocus, setIsMatchingPwdOnFocus] = useState(false);
+
+    const [ isSecretOnFocus, setIsSecretOnFocus ] = useState(false);
+
+    //? JSX refs
+    const usernameInput = useRef(null);
+
+    useEffect(() => {
+        usernameInput.current.focus();
+    }, []);
+
     return (
         <section className="register">
+            {error && 
             <div className="error-message">
-                <BiErrorAlt id = "error-icon"/>
+                <IoIosWarning id = "warning-icon"/>
                 {error}
-            </div>
+            </div>}
             <form action="">
-                <label for = "username">New username:</label>
+                <label for = "username">
+                    <p>New username:<span className="r-asterick">*</span></p>
+                    {!username ? null 
+                        : isUsernameValid 
+                            ? <FaCheckCircle className = "right"/>
+                            : <FaCircleXmark className = "wrong"/> }
+                </label>
                 <div className="input-container">
-                    <input id = "username" type="text" required/>
+                    <input 
+                        id = "username" 
+                        type="text" 
+                        ref = {usernameInput} 
+                        required
+                        value = {username}
+                        onChange = {(event) => setUsername(event.target.value)}
+                    />
+                    {username && !isUsernameValid ?
                     <div className="requirements">
                         <div className="first-line"><LuInfo className = "requirement-icon"/>3 to 24 characters</div>
                         <p>Must begin with a letter.</p>
+                        <p>No space.</p>
                         <p>Letters, numbers, underscores, hyphens allowed.</p>
-                    </div>
-                </div>
+                    </div> : null }
 
-                <label for = "password">New password:</label>
+                </div>
+                <label for = "password">
+                    <p>New password:<span className="r-asterick">*</span></p>
+                    {!password ? null 
+                        : isPasswordValid
+                            ? <FaCheckCircle className = "right"/>
+                            : <FaCircleXmark className = "wrong"/> }
+                </label>
                 <div className="input-container">
-                    <input id = "password" type="password" required/>
+                    <input 
+                        id = "password" 
+                        type="password" 
+                        required
+                        value = {password}
+                        onChange = {(event) => setPassword(event.target.value)}
+                    />
+                    {password && !isPasswordValid ?
                     <div className="requirements">
                         <div className="first-line"><LuInfo className = "requirement-icon"/>8 to 24 characters.</div>
                         <p>Must include uppercase and lowercase letters, a number and a special character.</p>
                         <p>Allowed special characters: @, #, $, %</p>
-                    </div>
+                    </div> : null }
+
                 </div>
 
-                <label for = "matching-password">Re-type password:</label>
+                <label for = "matching-password">
+                    <p>Re-type password:<span className="r-asterick">*</span></p>
+                </label>
                 <div className="input-container">
-                    <input id = "matching-password" type="password" required/>
+                    <input 
+                        id = "matching-password" 
+                        type="password" 
+                        required
+                        onFocus = {() => setIsMatchingPwdOnFocus(true)}
+                        onBlur = {() => setIsMatchingPwdOnFocus(false)}
+                    />
+                    {isMatchingPwdOnFocus &&                     
                     <div className="requirements">
-                        <div className="first-line"><LuInfo className = "requirement-icon"/>Must match the password above</div>
-                    </div>
+                            <div className="first-line"><LuInfo className = "requirement-icon"/>Must match the password above</div>
+                    </div> }
+
                 </div>
 
                 <label for = "secret">Secret</label>
                 <div className="input-container">
-                    <input id = "secret" type="passowrd"/>
+                    <input 
+                        id = "secret" 
+                        type="passowrd"
+                        onFocus = {() => setIsSecretOnFocus(true)}
+                        onBlur = {() => setIsSecretOnFocus(false)}
+                    />
+                    {isSecretOnFocus && 
                     <div className="requirements">
                         <div className="first-line"><LuInfo className = "requirement-icon"/>Secret key to create Admin account.</div>
-                    </div>
+                    </div> }
+
                 </div>
 
                 <Button hoverEffect = {false}>Register</Button>
